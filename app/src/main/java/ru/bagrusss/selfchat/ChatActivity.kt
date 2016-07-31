fbb7516c4d3275f4f18a4697120b46f7bd107354
bagrusss <bagrusss@gmail.com>
2016-07-31 18:50:49 +0300

    init

--- app/src/main/java/ru/bagrusss/selfchat/ChatActivity.kt
+++ app/src/main/java/ru/bagrusss/selfchat/ChatActivity.kt
@@ -2,8 +2,6 @@ package ru.bagrusss.selfchat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import org.jetbrains.anko.button
 import org.jetbrains.anko.editText
 import org.jetbrains.anko.hintResource
@@ -27,12 +25,6 @@ class ChatActivity : AppCompatActivity() {
                 textSize = 26f
             }
         }
-        /*setContentView(R.layout.activity_chat)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-
-        val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab!!.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }*/
     }
 
 }
